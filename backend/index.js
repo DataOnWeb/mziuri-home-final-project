8a6671269c6e9a9c09de5744929a6e120a66b4f6
Data <khachapuridzedata@gmail.com>
2025-06-25 21:20:12 +0400

    .

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,20 @@ app.use(limiter)
 
 
 
-app.use(cors());
+const allowedOrigins = [
+  'https://pronia-app.onrender.com'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
